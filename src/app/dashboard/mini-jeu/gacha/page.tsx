eaf1fa85d99b3cffcbffec51c9d564755e2c22a7
Kyusaan41 <kyusan_lol@outlook.fr>
2025-10-25 22:57:26 +0200

    fix

--- src/app/dashboard/mini-jeu/gacha/page.tsx
+++ src/app/dashboard/mini-jeu/gacha/page.tsx
@@ -6,7 +6,7 @@ import GachaLoader from './GachaLoader';
 
 export default function GachaPage() {
   return (
-    <WithMaintenanceCheck pageId="gacha">
+    <WithMaintenanceCheck pageId="mini-jeu-gacha">
       {() => {
         const GachaClientPage = dynamic(() => import('./GachaClientPage'), { ssr: false });
         return <GachaClientPage />;
